f9528ebf5df7c01986a43f7e82d523d2cb516ddb
pc <you@example.com>
2019-05-19 10:21:13 -0300

    nuevo system out

--- Ej2/src/main/java/Main.java
+++ Ej2/src/main/java/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here}
         System.out.println("Ej2 ") ;
+        System.out.println("ola k ase ") ;
     }
     
 }
