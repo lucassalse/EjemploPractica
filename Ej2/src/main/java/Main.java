b4e0b40ba1803b35d93c148894d3ceb9ee76b07b
sebastian <sebastian@sebastian-linux>
2019-05-21 00:38:38 -0300

    agregué rama nueva (inspirada en aymeDIOS)

--- Ej2/src/main/java/Main.java
+++ Ej2/src/main/java/Main.java
@@ -26,6 +26,8 @@ public class Main {
         
         System.out.println("EL METODO DE LA SACUDIDA EN RAMA LOCAL");
         
+        Wanchope respuesta = new Wanchope() ;
+        System.out.println(respuesta.getFraseWanchopista());
 
     }
     
